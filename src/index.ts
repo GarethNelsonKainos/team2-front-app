8d180288f721d40afbb1775938d69ef3c5c48b1e
DavidOHanlon1985 <david.o'hanlon@kainos.com>
2026-02-09 14:54:24 +0000

    Port moved to .env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
