334dc1d76e86c5c46373104175ba669b2d8c8efb
lauren5656 <lauren.wilson@kainos.com>
2026-02-12 16:28:01 +0000

    login error handling

--- src/UI/controllers/loginController.ts
+++ src/UI/controllers/loginController.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from "express";
 import { loginUser } from "../services/loginService";
+import { error } from "node:console";
 
 export class LoginController {
-    async handleLogin(req: Request, res: Response) {
-        try {
-            const { email, password } = req.body;
-            const data = await loginUser(email, password);
-            if (data && data.token) {
-                res.render("logged-in");
-                console.log("Login successful, token received:", data.token);
-            } else {
-                res.status(401).send("Login failed: Invalid response.");
-            }
-        } catch (error) {
-            res.status(500).send("Server error during login.");
-        }
+  async handleLogin(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      console.log(email, password);
+      const data = await loginUser(email, password);
+      if (data && data.token) {
+        res.render("home-page", { showLoginModal: false });
+        console.log("Login successful, token received:", data.token);
+      } else {
+        res.status(401).render("home-page", { showLoginModal: true, error: "Invalid email or password." });
+      }
+    } catch (error) {
+      res.status(500).send("Server error during login.");
     }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,10 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', UIRoutes);
+
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
 res.render('home-page', { title: 'Home' });
