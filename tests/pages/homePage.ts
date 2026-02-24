273c912253d89f4cc5841d2df3ca6b670f3c6c27
Nick Parke <nicholas.parke@kainos.com>
2026-02-24 16:02:26 +0000

    Run linting and formatting

--- tests/pages/homePage.ts
+++ tests/pages/homePage.ts
@@ -1,17 +1,17 @@
-import type { Locator, Page } from '@playwright/test';
- 
-export class HomePage {
-  constructor(private readonly page: Page) {}
- 
-  welcomeHeading(name: string): Locator {
-    return this.page.getByRole('heading', { name: `Welcome, ${name}!` });
-  }
- 
-  logoutLink(): Locator {
-    return this.page.getByRole('link', { name: 'Logout' });
-  }
+import type { Locator, Page } from "@playwright/test";
 
-  adminDashboardLink(): Locator {
-    return this.page.getByRole('link', { name: 'Admin Dashboard' });
-  }
+export class HomePage {
+	constructor(private readonly page: Page) {}
+
+	welcomeHeading(name: string): Locator {
+		return this.page.getByRole("heading", { name: `Welcome, ${name}!` });
+	}
+
+	logoutLink(): Locator {
+		return this.page.getByRole("link", { name: "Logout" });
+	}
+
+	adminDashboardLink(): Locator {
+		return this.page.getByRole("link", { name: "Admin Dashboard" });
+	}
 }
