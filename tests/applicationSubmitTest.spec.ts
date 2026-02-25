06edf0dec806697a290d1d28ecf35c3ac98cb062
SamTougherKainos <samuel.tougher@Kainos.com>
2026-02-25 16:27:54 +0000

    application submit test playwright

---
+++ tests/applicationSubmitTest.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "./pages/homePage";
+import { JobRolePage } from "./pages/jobRolePage";
+import { AuthPage } from "./pages/authPage";
+import { ApplicationPage } from "./pages/applicationPage";
+
+test.describe("Application Submission", () => {
+        
+    test.beforeEach(async ({ page }) => {
+        const email = process.env.PLAYWRIGHT_USER_USERNAME || "";
+        const password = process.env.PLAYWRIGHT_USER_PASSWORD || "";
+        const authPage = new AuthPage(page);
+        await authPage.gotoLogin();
+		await authPage.fillLoginForm({
+			email: email,
+			password: password,
+		});
+		await authPage.submitLogin();
+    });
+
+    test('User will login and press view my applications then verify that the page exists', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const applicationPage = new ApplicationPage(page);
+        await homePage.gotoUserApplications();
+        await applicationPage.checkMyRecentApplications();
+    });
+
+    test('User will login and create and application then view it', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const applicationPage = new ApplicationPage(page);
+        const jobRolePage = new JobRolePage(page);
+        await homePage.goToAllJobRoles();
+        await jobRolePage.openJobRole();
+        await applicationPage.applyForRole();
+        //await page.waitForURL(/\/apply/);
+        await applicationPage.populateApplicationForm();
+
+        
+        //await applicationPage.checkMyRecentApplicationExists();  
+    });
+});
---
+++ tests/pages/applicationPage.ts
@@ -0,0 +1,37 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+import { promises } from "node:dns";
+
+export class ApplicationPage {
+    readonly myRecentApplications: Locator;
+    readonly applyNowLink: Locator;
+    readonly chooseFileButton: Locator;
+    readonly submitButton: Locator;
+    readonly applicationSubmittedText: Locator;
+
+    constructor(private readonly page: Page) {
+        this.myRecentApplications = this.page.getByText("My Recent Applications");
+        this.applyNowLink = this.page.getByRole("link", { name: "Apply Now" });
+        this.chooseFileButton = this.page.getByRole('button', { name: 'Choose file to upload' });
+        this.submitButton = this.page.getByRole('button', { name: 'Submit' });
+        this.applicationSubmittedText = this.page.getByText('Application submitted');
+    }
+
+    async checkMyRecentApplications(): Promise<Boolean> {
+        return await this.myRecentApplications.isVisible();
+    }
+
+    checkMyRecentApplicationExists(applicationName: string): Locator {
+        return this.page.getByRole("row", { name: applicationName });
+    }
+
+    async applyForRole(): Promise<void> {
+        await this.applyNowLink.click();
+    }
+
+    async populateApplicationForm(): Promise<void> {
+        await this.chooseFileButton.click();
+        await this.chooseFileButton.setInputFiles('/Users/sam/Downloads/Bee-Movie-2007.pdf');
+        await this.submitButton.click();
+        await this.applicationSubmittedText.isVisible();
+    }
+}
