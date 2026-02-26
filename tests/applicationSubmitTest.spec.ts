b05e17b85db4e761c019951171fe2c8cded8973e
SamTougherKainos <samuel.tougher@Kainos.com>
2026-02-26 09:27:57 +0000

    formatting applied

--- tests/applicationSubmitTest.spec.ts
+++ tests/applicationSubmitTest.spec.ts
@@ -5,42 +5,51 @@ import { AuthPage } from "./pages/authPage";
 import { ApplicationPage } from "./pages/applicationPage";
 
 test.describe("Application Submission", () => {
-        
-    test.beforeEach(async ({ page }) => {
-        const email = process.env.PLAYWRIGHT_USER_USERNAME || "";
-        const password = process.env.PLAYWRIGHT_USER_PASSWORD || "";
-        const authPage = new AuthPage(page);
-        await authPage.gotoLogin();
+	test.beforeEach(async ({ page }) => {
+		const email = process.env.PLAYWRIGHT_USER_USERNAME || "";
+		const password = process.env.PLAYWRIGHT_USER_PASSWORD || "";
+		const authPage = new AuthPage(page);
+		await authPage.gotoLogin();
 		await authPage.fillLoginForm({
 			email: email,
 			password: password,
 		});
 		await authPage.submitLogin();
-    });
+	});
 
-    test('User will login and press view my applications then verify that the page exists', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const applicationPage = new ApplicationPage(page);
-        await homePage.gotoUserApplications();
-        await applicationPage.checkMyRecentApplications();
-    });
+	test("User will login and press view my applications then verify that the page exists", async ({
+		page,
+	}) => {
+		const homePage = new HomePage(page);
+		const applicationPage = new ApplicationPage(page);
+		await homePage.gotoUserApplications();
+		await applicationPage.checkMyRecentApplications();
+	});
 
-    test('User will login and create and application then view it', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const applicationPage = new ApplicationPage(page);
-        const jobRolePage = new JobRolePage(page);
-        await homePage.goToAllJobRoles();
-        await jobRolePage.openJobRole();
-        await applicationPage.applyForRole();
-        await applicationPage.populateApplicationForm();
+	test("User will login and create and application then view it", async ({
+		page,
+	}) => {
+		const homePage = new HomePage(page);
+		const applicationPage = new ApplicationPage(page);
+		const jobRolePage = new JobRolePage(page);
+		await homePage.goToAllJobRoles();
+		await jobRolePage.openJobRole();
+		await applicationPage.applyForRole();
+		await applicationPage.populateApplicationForm();
 
-        await expect.poll(() => applicationPage.checkApplicationSubmitted()).toBe(true);
-    });
+		await expect
+			.poll(() => applicationPage.checkApplicationSubmitted())
+			.toBe(true);
+	});
 
-    test('User will login and view their applications and verify that the application they just submitted exists', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const applicationPage = new ApplicationPage(page);
-        await homePage.gotoUserApplications();
-        await expect.poll(() => applicationPage.checkMyRecentApplicationExists()).toBe(true);
-    });
+	test("User will login and view their applications and verify that the application they just submitted exists", async ({
+		page,
+	}) => {
+		const homePage = new HomePage(page);
+		const applicationPage = new ApplicationPage(page);
+		await homePage.gotoUserApplications();
+		await expect
+			.poll(() => applicationPage.checkMyRecentApplicationExists())
+			.toBe(true);
+	});
 });
--- tests/pages/applicationPage.ts
+++ tests/pages/applicationPage.ts
@@ -2,40 +2,44 @@ import { expect, type Locator, type Page } from "@playwright/test";
 import { promises } from "node:dns";
 
 export class ApplicationPage {
-    readonly myRecentApplications: Locator;
-    readonly applyNowLink: Locator;
-    readonly chooseFileButton: Locator;
-    readonly submitButton: Locator;
-    readonly applicationSubmittedText: Locator;
-    readonly applicationStatus: Locator;
+	readonly myRecentApplications: Locator;
+	readonly applyNowLink: Locator;
+	readonly chooseFileButton: Locator;
+	readonly submitButton: Locator;
+	readonly applicationSubmittedText: Locator;
+	readonly applicationStatus: Locator;
 
-    constructor(private readonly page: Page) {
-        this.myRecentApplications = this.page.getByText("My Recent Applications");
-        this.applyNowLink = this.page.getByRole("link", { name: "Apply Now" });
-        this.chooseFileButton = this.page.getByRole('button', { name: 'Choose file to upload' });
-        this.submitButton = this.page.getByRole('button', { name: 'Submit' });
-        this.applicationSubmittedText = this.page.getByText('Application submitted');
-        this.applicationStatus = this.page.getByRole('row').getByText('Submitted');
-    }
+	constructor(private readonly page: Page) {
+		this.myRecentApplications = this.page.getByText("My Recent Applications");
+		this.applyNowLink = this.page.getByRole("link", { name: "Apply Now" });
+		this.chooseFileButton = this.page.getByRole("button", {
+			name: "Choose file to upload",
+		});
+		this.submitButton = this.page.getByRole("button", { name: "Submit" });
+		this.applicationSubmittedText = this.page.getByText(
+			"Application submitted",
+		);
+		this.applicationStatus = this.page.getByRole("row").getByText("Submitted");
+	}
 
-    async checkMyRecentApplications(): Promise<Boolean> {
-        return await this.myRecentApplications.isVisible();
-    }
+	async checkMyRecentApplications(): Promise<Boolean> {
+		return await this.myRecentApplications.isVisible();
+	}
 
-    async applyForRole(): Promise<void> {
-        await this.applyNowLink.click();
-    }
+	async applyForRole(): Promise<void> {
+		await this.applyNowLink.click();
+	}
 
-    async populateApplicationForm(): Promise<void> {
-        await this.chooseFileButton.click();
-        await this.chooseFileButton.setInputFiles('TEST_CV.pdf');
-        await this.submitButton.click();
-    }
-    async checkApplicationSubmitted(): Promise<Boolean> {
-        return await this.applicationSubmittedText.isVisible();
-    }
+	async populateApplicationForm(): Promise<void> {
+		await this.chooseFileButton.click();
+		await this.chooseFileButton.setInputFiles("TEST_CV.pdf");
+		await this.submitButton.click();
+	}
+	async checkApplicationSubmitted(): Promise<Boolean> {
+		return await this.applicationSubmittedText.isVisible();
+	}
 
-    async checkMyRecentApplicationExists(): Promise<Boolean> {
-        return await this.applicationStatus.isVisible();
-    }
+	async checkMyRecentApplicationExists(): Promise<Boolean> {
+		return await this.applicationStatus.isVisible();
+	}
 }
